63900ee4528efac065eba929684044fa31e185a6
baharkose <baharkse17@gmail.com>
2023-11-10 20:38:19 +0300

    finished basic event and calculating

--- app.js
+++ app.js
@@ -8,13 +8,27 @@ let yourIncome = document.querySelector("#geliriniz")
 let yourSpend = document.querySelector("#gideriniz")
 let yourSaving = document.querySelector("#kalan")
 const spendBody  = document.querySelector("#harcama-body")
+const clearBtn  = document.querySelector("#temizle-btn")
+const result  = document.querySelector(".result")
+
+let incomeT = 0;
+
 
 
 addBtn.addEventListener("click",()=>{
+let a = income.value
+    if(isNaN(a)){
+        alert("Please enter a valid income")
+        
+    }
+    else{incomeT = yourIncome.textContent = income.value;
+        calculateSpend(incomeT)
+        console.log("is clicked");
+        console.log(yourIncome.textContent);
+    }
+    
 
-    console.log("is clicked");
-    console.log(yourIncome.textContent);
-    yourIncome.textContent = income.value;
+    
 
 })
 
@@ -22,6 +36,7 @@ saveBtn.addEventListener("click", ()=>{
     let tr = document.createElement("tr")
 
     let trNew = spendBody.appendChild(tr)
+    trNew.className = "trNew"
 
     let tdDate = document.createElement("td")
     tdDate.textContent = date.value
@@ -40,11 +55,21 @@ saveBtn.addEventListener("click", ()=>{
     let trashI = document.createElement("i") 
     trashI.className="fa-solid";
     trashI.className+=" fa-trash";
+    trashI.style.cursor = "pointer";
     tdTrash.appendChild(trashI)
+
+
+
+    trashI.addEventListener("click", ()=>{
+        console.log("is clicked");
+        trashI.closest(".trNew").remove();
+        calculateSpend(trNew);
+        // trashI.closest(".trNew").tdQuantity.textContent = "";
+    })
    
     trNew.appendChild(tdTrash, trNew)
 
-    calculateSpend(trNew)
+    calculateSpend(incomeT)
 
     // <i class="fa-solid fa-trash"></i>
     
@@ -53,16 +78,42 @@ saveBtn.addEventListener("click", ()=>{
 })
 
 
-const calculateSpend = (trNew)=>{
+
+const calculateSpend = (incomeT)=>{
+
+    // incomeT = 0;
+    console.log(incomeT);
     let sumQuantity = 0;
     let allQuantity =  [...spendBody.querySelectorAll(".quantityN")]
-    
-    
-    
     let sumAll = allQuantity.reduce((sum, item)=>sum + Number(item.textContent), 0);
     console.log(sumAll);
  
     yourSpend.textContent = sumAll;
 
+    let totalSavedMoney = incomeT - sumAll;
+        yourSaving.textContent = totalSavedMoney;
+        
+        let span = result.createElement("span")
+        result.appendChild(span);
+        span.className = "bg-danger"
+
+
+    span.textContent = ` ${date.value}`
+    result.textContent= ` tarihli toplam birikim tutarınız ${totalSavedMoney}'dir. `;
+
+    return yourSaving.textContent;
+    }
+
+    
+    
     // console.log(allQuantity);
-}
+    clearBtn.addEventListener("click", ()=>{
+    income.value = "";
+    yourSpend.textContent = ""
+    yourIncome.textContent = ""
+    spendArea.value = "";
+    date.value = ""; //
+    quantity.value = ""
+    yourSaving.textContent = ""
+})
+
